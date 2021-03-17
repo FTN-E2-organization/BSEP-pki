2379116e30018dbb3878b4c332ae97ce6c8123bb
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-03-17 16:44:35 +0100

    feat:project initialized

---
+++ pki/src/main/java/rs/ac/uns/ftn/bsep/pki/PkiApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bsep.pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiApplication.class, args);
+	}
+
+}
