74862624aa1b88f7120b295ff488142c5c38e1cd
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-03-21 19:19:22 +0100

    gui: view issuer of the certificate

--- pki/src/main/resources/static/js/admin_show_all.js
+++ pki/src/main/resources/static/js/admin_show_all.js
@@ -24,7 +24,7 @@ function addRowInTable(c){
 	
 	let btnCheckValidity = '<button data-toggle="modal" data-target="#centralModalCheckValidation" class="btn btn-info btn-sm" type="button" id="' + c.id +'" onclick="validation(this.id)">Validity</button>';
 	let btnRevoke = '<button class="btn btn-danger btn-sm" type="button" id="' + c.id +'" onclick="revoke(this.id)">Revoke</button>';
-	let btnIssuer = '<button class="btn btn-info btn-sm" type="button" id="' + c.id +'" onclick="getIssuer(this.id)">Issuer</button>';
+	let btnIssuer = '<button data-toggle="modal" data-target="#centralModalViewIssuer" class="btn btn-info btn-sm" type="button" id="' + c.issuerId +'" onclick="getIssuer(this.id)">Issuer</button>';
 	
 	let row = $('<tr><td>' + c.organization + '</td><td>' + c.organizationUnit + '</td>' +
 				'<td>' + c.commonName + '</td><td>' + c.givenName + '</td><td>' + c.surname + '</td>' +
@@ -47,10 +47,10 @@ function validation(certificateId) {
         },
 		contentType: "application/json",
 		success:function(isValid){
-			if(isValid = true){
-				document.getElementById('idCheckValidation').innerHTML = 'The certificate is valid!';
+			if(isValid == true){
+				document.getElementById('idCheckValidation').innerHTML = '<h5 style="color:green;">The certificate is valid!</h5>';
 			}else{
-				document.getElementById('idCheckValidation').innerHTML = 'The certificate is not valid!';
+				document.getElementById('idCheckValidation').innerHTML = '<h5 style="color:red;">The certificate is not valid!</h5>';
 			}
 		},
 		error:function(){
@@ -65,6 +65,39 @@ function revoke(certificateId) {
 
 };
 
-function getIssuer(certificateId) {
+function getIssuer(issuerId) {
 
+	$('#div_info').empty();
+	$.ajax({
+		type:"GET", 
+		url: "/api/certificate/" + issuerId,
+		headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        },
+		contentType: "application/json",
+		success:function(certificate){
+			let table_info =  '<table style="margin-top:30px; margin-bottom:30px;">'
+						+ '<tr> <td>Organization:</td><td>' + certificate.organization + '</td> </tr>' 
+						+ '<tr> <td>Organization unit:</td><td>' + certificate.organizationUnit + '</td> </tr>' 
+						+ '<tr> <td>Common name:</td><td>' + certificate.commonName + '</td></tr>' 
+						+ '<tr> <td>Given name:</td><td>' + certificate.givenName + '</td> </tr>'
+						+ '<tr> <td>Surname:</td><td>' + certificate.surname + '</td> </tr>' 
+						+ '<tr> <td>Email:</td><td>' + certificate.email + '</td> </tr>' 
+						+ '<tr> <td>Country code:</td><td>' + certificate.countryCode + '</td> </tr>' 
+						+ '<tr> <td>State:</td><td>' + certificate.state + '</td> </tr>' 
+						+ '<tr> <td>Locality:</td><td>' + certificate.locality + '</td> </tr>' 
+						+ '<tr> <td>Start date:</td><td>' + certificate.startDate + '</td> </tr>' 
+						+ '<tr> <td>End date:</td><td>' + certificate.startDate + '</td> </tr>' 
+						+ '<tr> <td>CA:</td><td>' + certificate.isCA + '</td> </tr>' 
+						+ '<tr> <td>Is revoked:</td><td>' + certificate.isRevoked + '</td> </tr>' 
+                        + '</table>';
+                        
+            $(div_info).append(table_info);
+            document.getElementById("div_info").style.display = "initial";
+		},
+		error:function(){
+			console.log('error getting certificate');
+		}
+	});
+	
 };
