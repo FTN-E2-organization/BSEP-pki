cc8f17a802025000374539feb049f8ed8fb321a7
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-03-21 22:25:14 +0100

    gui:modals for validity and issuer added

--- pki/src/main/resources/static/js/subject_show_all.js
+++ pki/src/main/resources/static/js/subject_show_all.js
@@ -24,8 +24,8 @@ $(document).ready(function () {
 function addRowInTable(c){
 	
 	let btnDownload = '<button class="btn btn-info" type="button" id="' + c.id +'" onclick="download(this.id)" style="margin-top:-10px; margin-left:-20px; margin-right:-20px;">Download</button>';
-	let btnCheckValidity = '<button class="btn btn-info" type="button" id="' + c.id +'" onclick="checkValidity(this.id)" style="margin-top:-10px;  margin-left:-20px; margin-right:-20px;">Validity</button>';
-	let btnIssuer = '<button class="btn btn-info" type="button" id="' + c.id +'" onclick="getIssuer(this.id)" style="margin-top:-10px;  margin-left:-20px; margin-right:-20px;">Issuer</button>';
+	let btnCheckValidity = '<button data-toggle="modal" data-target="#centralModalCheckValidation" class="btn btn-info" type="button" id="' + c.id +'" onclick="checkValidity(this.id)" style="margin-top:-10px;  margin-left:-20px; margin-right:-20px;">Validity</button>';
+	let btnIssuer = '<button data-toggle="modal" data-target="#centralModalViewIssuer" class="btn btn-info" type="button" id="' + c.id +'" onclick="getIssuer(this.id)" style="margin-top:-10px;  margin-left:-20px; margin-right:-20px;">Issuer</button>';
 	
 	let row = $('<tr><td>' + c.startDate + '</td><td>' + c.endDate + '</td>' + 
 				'<td>' + c.isCA + '</td><td>' + c.isRevoked + '</td><td>' + btnCheckValidity + '</td>' +
@@ -60,9 +60,59 @@ function download(certificateId){
 }
 
 function checkValidity(certificateId){
-	
+	$.ajax({
+		type:"GET", 
+		url: "/api/certificate/" + certificateId + "/valid",
+		headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        },
+		contentType: "application/json",
+		success:function(isValid){
+			if(isValid == true){
+				document.getElementById('idCheckValidation').innerHTML = '<h5 style="color:green;">The certificate is valid!</h5>';
+			}else{
+				document.getElementById('idCheckValidation').innerHTML = '<h5 style="color:red;">The certificate is not valid!</h5>';
+			}
+		},
+		error:function(){
+			console.log('error getting certificate validation');
+		}
+	});
 };
 
-function btnIssuer(certificateId){
+function getIssuer(issuerId){
+	
+	$('#div_info').empty();
+	$.ajax({
+		type:"GET", 
+		url: "/api/certificate/" + issuerId,
+		headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        },
+		contentType: "application/json",
+		success:function(certificate){
+			let table_info =  '<table style="margin-top:30px; margin-bottom:30px;">'
+						+ '<tr> <td>Organization:</td><td>' + certificate.organization + '</td> </tr>' 
+						+ '<tr> <td>Organization unit:</td><td>' + certificate.organizationUnit + '</td> </tr>' 
+						+ '<tr> <td>Common name:</td><td>' + certificate.commonName + '</td></tr>' 
+						+ '<tr> <td>Given name:</td><td>' + certificate.givenName + '</td> </tr>'
+						+ '<tr> <td>Surname:</td><td>' + certificate.surname + '</td> </tr>' 
+						+ '<tr> <td>Email:</td><td>' + certificate.email + '</td> </tr>' 
+						+ '<tr> <td>Country code:</td><td>' + certificate.countryCode + '</td> </tr>' 
+						+ '<tr> <td>State:</td><td>' + certificate.state + '</td> </tr>' 
+						+ '<tr> <td>Locality:</td><td>' + certificate.locality + '</td> </tr>' 
+						+ '<tr> <td>Start date:</td><td>' + certificate.startDate + '</td> </tr>' 
+						+ '<tr> <td>End date:</td><td>' + certificate.startDate + '</td> </tr>' 
+						+ '<tr> <td>CA:</td><td>' + certificate.isCA + '</td> </tr>' 
+						+ '<tr> <td>Is revoked:</td><td>' + certificate.isRevoked + '</td> </tr>' 
+                        + '</table>';
+                        
+            $(div_info).append(table_info);
+            document.getElementById("div_info").style.display = "initial";
+		},
+		error:function(){
+			console.log('error getting certificate');
+		}
+	});
 	
 };
