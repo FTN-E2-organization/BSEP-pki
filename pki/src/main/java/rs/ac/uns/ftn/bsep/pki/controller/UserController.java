17eeb85386b200c2ad939b7535663959ae1e28a2
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-04-21 18:45:44 +0200

    refactor:delete pki-frontend repository

--- pki/src/main/java/rs/ac/uns/ftn/bsep/pki/controller/UserController.java
+++ pki/src/main/java/rs/ac/uns/ftn/bsep/pki/controller/UserController.java
@@ -1,18 +1,16 @@
 package rs.ac.uns.ftn.bsep.pki.controller;
 
 import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import rs.ac.uns.ftn.bsep.pki.dto.AddUserDTO;
 import rs.ac.uns.ftn.bsep.pki.dto.UserDTO;
 import rs.ac.uns.ftn.bsep.pki.exception.BadRequestException;
--- pki/src/main/resources/data-postgres.sql
+++ pki/src/main/resources/data-postgres.sql
@@ -3,9 +3,9 @@
 INSERT INTO authority (id,name) VALUES (1,'ROLE_ADMIN');
 INSERT INTO authority (id,name) VALUES (2,'ROLE_SUBJECT');
 
-/*
+
 INSERT INTO users (id, username, password,authority_id) VALUES ( 1,'admin','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',1);
-INSERT INTO users (id, username, password,authority_id) VALUES ( 2,'ftn.uns.ac.rs','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',2);
+/*INSERT INTO users (id, username, password,authority_id) VALUES ( 2,'ftn.uns.ac.rs','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',2);
 INSERT INTO users (id, username, password,authority_id) VALUES ( 3,'marija.maric98','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',2);
 INSERT INTO users (id, username, password,authority_id) VALUES ( 4,'pmf.uns.ac.rs','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',2);
 INSERT INTO users (id, username, password,authority_id) VALUES ( 5,'pera_peric99','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',2);
