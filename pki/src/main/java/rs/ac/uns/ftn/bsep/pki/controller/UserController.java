8875b1a0806ddf3b6542f62ce0459c4af7d70e32
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-04-21 14:27:23 +0200

    feat:validation on backend side for registration

--- pki/src/main/java/rs/ac/uns/ftn/bsep/pki/controller/UserController.java
+++ pki/src/main/java/rs/ac/uns/ftn/bsep/pki/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import rs.ac.uns.ftn.bsep.pki.dto.AddUserDTO;
 import rs.ac.uns.ftn.bsep.pki.dto.UserDTO;
 import rs.ac.uns.ftn.bsep.pki.exception.BadRequestException;
+import rs.ac.uns.ftn.bsep.pki.exception.ValidationException;
 import rs.ac.uns.ftn.bsep.pki.mapper.UserMapper;
 import rs.ac.uns.ftn.bsep.pki.service.UserService;
+import rs.ac.uns.ftn.bsep.pki.validator.UserValidator;
 
 @RestController
 @RequestMapping(value = "api/user")
@@ -41,18 +44,21 @@ public class UserController {
 		}
 	}
 	
-	@RequestMapping(path = "/subjects",  method = RequestMethod.POST, consumes = "application/json")
+	@PostMapping(value = "/subjects", consumes = "application/json")
 	public ResponseEntity<?> addSubject(@RequestBody AddUserDTO userDTO){
 		try {
+			UserValidator.addSubjectValidator(userDTO);
 			userService.addSubject(userDTO);
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		catch (ValidationException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 		catch (BadRequestException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 		catch (Exception e) {
-			String msg = "An error occurred while sending request for registration.";
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>("An error occurred while sending request for registration.", HttpStatus.BAD_REQUEST);
 		}
 	}
 }
