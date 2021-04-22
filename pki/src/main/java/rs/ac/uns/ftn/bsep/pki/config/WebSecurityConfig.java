8413a756847c5e7c50cedecd08d55f417fb062e5
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-04-23 01:37:15 +0200

    refactor: refactor xss protection

--- pki/src/main/java/rs/ac/uns/ftn/bsep/pki/config/WebSecurityConfig.java
+++ pki/src/main/java/rs/ac/uns/ftn/bsep/pki/config/WebSecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import rs.ac.uns.ftn.bsep.pki.security.auth.RestAuthenticationEntryPoint;
 import rs.ac.uns.ftn.bsep.pki.security.auth.TokenAuthenticationFilter;
@@ -59,26 +58,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 
-				.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+		.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
 
-				.authorizeRequests()
-					.antMatchers("/auth/**").permitAll()
-					.antMatchers("/h2-console/**").permitAll()
-					.antMatchers("/api/auth/confirm-account").permitAll()  //aktiviranje naloga preko mejla
-				.anyRequest().authenticated().and()
-				.cors().and()
-				.addFilterBefore(new TokenAuthenticationFilter(tokenUtils, jwtUserDetailsService),
-
-						BasicAuthenticationFilter.class)
+		.authorizeRequests()
+			.antMatchers("/auth/**").permitAll()
+			.antMatchers("/h2-console/**").permitAll()
+			.antMatchers("/api/auth/confirm-account").permitAll()  //aktiviranje naloga preko mejla
+		.anyRequest().authenticated().and()
+		.cors().and()
+		.addFilterBefore(new TokenAuthenticationFilter(tokenUtils, jwtUserDetailsService),
+				BasicAuthenticationFilter.class)
 		//XSS protection
         .headers()
         .xssProtection()
         .and()
         .contentSecurityPolicy("script-src 'self'");
 		
-		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		http.csrf().disable();
 		http.headers().frameOptions().disable();
 
 	}
