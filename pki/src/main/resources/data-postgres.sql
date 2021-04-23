79ddf88ac01ce6cae319bb74f79574608b4e0efd
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-04-23 02:07:40 +0200

    refactor: modified SQL file

--- pki/src/main/resources/data-postgres.sql
+++ pki/src/main/resources/data-postgres.sql
@@ -28,7 +28,7 @@ INSERT INTO AUTHORITIES_PERMISSIONS (authority_id, permission_id) VALUES (2,9);
 INSERT INTO AUTHORITIES_PERMISSIONS (authority_id, permission_id) VALUES (2,10);
 
 /* user kom je istekao link za aktiviranje naloga: */
-INSERT INTO users (id, username, password,authority_id, enabled, salt) VALUES (nextval('users_seq'),'jelenazeljko@live.com','$2y$10$cbn.yFXEQMvDBnZ3HrWoaeQgrRzknddNV9/.Vi1OWFihcvbCGqafi',2, false, '1234567k');
+INSERT INTO users (id, username, password,authority_id, enabled, salt) VALUES (nextval('users_seq'),'isa.user.test@gmail.com','$2y$10$cbn.yFXEQMvDBnZ3HrWoaeQgrRzknddNV9/.Vi1OWFihcvbCGqafi',2, false, '1234567k');
 INSERT INTO confirmation_token (token_id, confirmation_token, creation_date, user_id) VALUES (10, 'f819e980-58c6-415f-8908-f602ddd0b773', '2021-04-10', 1);
 
 INSERT INTO users (id, username, password,authority_id, enabled, salt) VALUES (nextval('users_seq'),'admin','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',1, true, '1234567k');
